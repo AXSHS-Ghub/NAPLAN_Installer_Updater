6e57077855603da77509c49ea88182916db7d2d7
AXSHS-Ghub <ben.percival@education.wa.edu.au>
2026-01-28 09:45:42 +0800

    Fix case sensitivity in signer check for JANISON

--- MacOS/bin/InstallNaplan.sh
+++ MacOS/bin/InstallNaplan.sh
@@ -197,7 +197,7 @@ install_naplan_ldb() {
     # Check the certificate name
     signer=$(pkgutil --check-signature "$PKG_PATH" | grep "Developer ID Installer" | awk -F': ' '{print $2}')
 
-    if [[ "$signer" != *"JANISON"* ]]; then
+    if [[ "$signer" != *"Janison"* ]]; then
     echo "WARNING: PKG is NOT signed by JANISON. Exiting."
         exit 1
     fi
