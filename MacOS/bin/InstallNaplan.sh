d6d54c6a90858c44e730434349a8c27583448e48
AXSHS-Ghub <ben.percival@education.wa.edu.au>
2026-01-28 10:02:46 +0800

    Update internet check to ping local address first

--- MacOS/bin/InstallNaplan.sh
+++ MacOS/bin/InstallNaplan.sh
@@ -94,7 +94,9 @@ check_internet () {
 # Ensure we have internet
 ping -c 1 8.8.8.8 &>/dev/null
 if [ $? -ne 0 ]; then
-    echo "No internet connection. Exiting." >> $LOG_FILE
+    ping -c 10.14.24.1 &>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "No internet connection. Exiting." >> $LOG_FILE
     exit 1
 fi
 }
